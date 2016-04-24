2ce44166e334b6c1ba61638faf811596e914c61a
Cybermar95 <dalamar95@gmail.com>
2016-04-24 18:08:17 +0300

    Revert "Revert "Merge pull request #1 from Cybermar95/Refactoring""
    This reverts commit 4e68219c2ba3573de5566714280e8d159226bdd7.

---
+++ Mediator/Mediator/Fhir/DataElement/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhir.DataElement
+{
+    class Extension
+    {
+        public string url;
+        public string valueString;
+        public Fhir.DataElement.Coding valueCoding;
+    }
+
+}
